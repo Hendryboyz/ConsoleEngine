d66ec97b6c87c02e1444ac122dc1cd8fa87a0c00
ext_henryc <henry_chou@trendmicro.com>
2019-01-09 15:44:27 +0800

    Add project files.

---
+++ NetCoreConsoleEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreConsoleEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
